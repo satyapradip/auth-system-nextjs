4fde220ee887afc33b773a77a1543bc6cd34ef0d
Satyapradip <satyapradip7602@gmail.com>
2026-01-31 02:04:31 +0530

    added route for the signup

---
+++ src/app/api/users/signup/route.ts
@@ -0,0 +1,40 @@
+import {connectToDB} from "@/src/db/db"
+import User from "@/src/models/userModel";
+import { NextRequest, NextResponse} from "next/server";
+import bcrypt from "bcryptjs";
+
+
+connectToDB()
+
+export async function POST(request: NextRequest) {
+  try {
+    const reqBody =await request.json()
+    const {username, email, password} = reqBody
+    console.log(reqBody);
+    
+    const user = await User.findOne({email})
+    if(user) {
+      return NextResponse.json({message: "User already exists"})
+    }
+    
+    // hased password
+    const salt = await bcrypt.genSalt(10)
+    const hashedPassword = await bcrypt.hash(password, salt)
+    const newUser = new User({
+      username,
+      email,
+      password: hashedPassword
+    })
+    // save the user 
+    const savedUser = await newUser.save()
+    console.log(savedUser);
+    return NextResponse.json({
+      message: "User created successfully",
+      success: true,
+      savedUser
+    })
+  } catch (error: any) {
+    return NextResponse.json({message: error.message})
+  
+  }
+}
